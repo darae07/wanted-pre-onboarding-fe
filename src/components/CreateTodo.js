5cf76ab13589a9ee7c420d56fb6bc2f96f91459f
darae07 <leedr0730@gmail.com>
2022-08-21 13:30:52 +0900

    todo 생성

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { authorizedInstance } from "../api/api";
 
 const initialTodoValues = {
   todo: "",
@@ -16,13 +17,24 @@ export default function CreateTodo() {
     setValues(nextValues);
   };
 
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await authorizedInstance.post("todos", values);
+      console.log(response);
+    } catch (e) {
+      alert(e.response?.data?.message || e.message);
+    }
+  };
+
   return (
     <div className="CreateTodo">
-      <form>
+      <form onSubmit={submit}>
         <div>
           <label htmlFor="todo"></label>
           <input name="todo" value={values.todo} onChange={handleInput} />
         </div>
+        <button type="submit">추가</button>
       </form>
     </div>
   );
