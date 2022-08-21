dff9493bf299a4ae899fd7c8cffc3beca6dc6069
darae07 <leedr0730@gmail.com>
2022-08-21 13:04:49 +0900

    로그인시 리다이렉트

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
-import { Login } from "../components";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Auth } from "../components";
+import { getItem } from "../util/storage";
 
 export default function LoginPage() {
+  const access_token = getItem("access_token", null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (access_token) {
+      navigate("/todo");
+    }
+  }, []);
+
   return (
     <div className="LoginPage">
       <h1>로그인</h1>
-      <Login></Login>
+      <Auth type="signin" />
       <Link to="signup">회원가입</Link>
     </div>
   );
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,7 +1,21 @@
+import { Auth } from "../components";
+import { getItem } from "../util/storage";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 export default function SignUpPage() {
+  const access_token = getItem("access_token", null);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (access_token) {
+      navigate("/todo");
+    }
+  }, []);
+
   return (
     <div className="SignUpPage">
       <h1>회원가입</h1>
+      <Auth type="signup" />
     </div>
   );
 }
