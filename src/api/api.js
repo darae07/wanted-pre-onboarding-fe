518a42f7dc01bdb8753ed3a7e32f0e6643330855
darae07 <leedr0730@gmail.com>
2022-08-21 13:43:00 +0900

    axios 설정 변경

--- src/api/api.js
+++ src/api/api.js
@@ -10,27 +10,25 @@ const authorizedInstance = axios.create({
   baseURL: SERVICE_BASE_URL,
 });
 
-const setupAxiosInterceptors = (navigate) => {
-  authorizedInstance.interceptors.request.use(
-    async (config) => {
-      try {
-        const accessToken = getItem("access_token", null);
-        if (accessToken) {
-          config.headers["Authorization"] = `Bearer ${accessToken}`;
-        } else {
-          throw new axios.Cancel("로그인이 필요합니다.");
-        }
-        return config;
-      } catch (error) {
-        navigate(`/`);
-        alert(error.message);
-        return Promise.reject({ response: { data: { error } } });
+authorizedInstance.interceptors.request.use(
+  async (config) => {
+    try {
+      const accessToken = getItem("access_token", null);
+      if (accessToken) {
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+      } else {
+        throw new axios.Cancel("로그인이 필요합니다.");
       }
-    },
-    (error) => {
+      return config;
+    } catch (error) {
+      window.history.pushState(null, null, `/`);
+      alert(error.message);
       return Promise.reject({ response: { data: { error } } });
     }
-  );
-};
+  },
+  (error) => {
+    return Promise.reject({ response: { data: { error } } });
+  }
+);
 
-export { anonymousInstance, authorizedInstance, setupAxiosInterceptors };
+export { anonymousInstance, authorizedInstance };
