a819f2b90790d1963dcd3cfc10cd07d3f8c0eca4
darae07 <leedr0730@gmail.com>
2022-08-21 11:09:56 +0900

    라우터 설정

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+  return (
+    <div className="SignUpPage">
+      <h1>회원가입</h1>
+    </div>
+  );
+}
---
+++ src/pages/TodoListPage.js
@@ -0,0 +1,7 @@
+export default function TodoListPage() {
+  return (
+    <div>
+      <h1>TODO 리스트</h1>
+    </div>
+  );
+}
